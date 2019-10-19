4b3aaa7a5810188df1f2b244399a5e37bde0adac
asaduzzaman1517 <asaduzzaman1517@gmail.com>
2019-10-19 07:06:50 +0600

    try function next

---
+++ script.js
@@ -0,0 +1,115 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Javascript syntext list:
+// 1. Js Types:
+// 1.1 Number
+// 1.2 String
+// 1.3 Boolean
+// 1.4 Undefined
+// 1.5 Null
+// 1.6 Symbol (es6)
+// 1.7 Object
+
+// 2. Javascript Comparisons
+// 2.1 !==
+// 2.2 ===
+// 2.3 >=
+// 2.4 <=
+// 2.5 >
+// 2.6 <
+
+// 3. Variables
+// 3.1 var
+// 3.2 let
+// 3.3 const
+
+// 4 Conditions
+// 4.1 if
+// 4.2 else
+// 4.3 else if
+// 4.4 ternary operator
+// 4.5 switch
+
+// 5 Logical operators
+// 5.1 &&
+// 5.2 ||
+// 5.3 !
+
+// 6.Functions
+// 6.1 var a = function name() {}
+// 6.2 function name() {}
+// 6.3 return
+// 6.4 () =>
+
+// 7. Data structure
+// 7.1 Array
+// 7.2 Object
+
+// 8. Looping
+// 8.1 for
+// 8.2 while
+// 8.3 do
+// 8.4 forEach
+
+// 9. JS Keywords
+// 9.1 case
+// 9.2 catch
+// 9.3 class
+// 9.4 const
+// 9.5 continue
+// 9.6 debugger
+// 9.7 default
+// 9.8 delete
+// 9.9 do
+// 9.10 else
+// 9.11 export
+// 9.12 extends
+// 9.13 finally
+// 9.14 for
+// 9.15 function
+// 9.16 if
+// 9.17 import
+// 9.18 in
+// 9.19 instanceof
+// 9.20 new
+// 9.21 return
+// 9.22 super
+// 9.23 switch
+// 9.24 this
+// 9.25 throw
+// 9.26 try
+// 9.27 typeof
+// 9.28 var
+// 9.29 void
+// 9.30 while
+// 9.31 with
+// 9.32 yield
+// 10. 
